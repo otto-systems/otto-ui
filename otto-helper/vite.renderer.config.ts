76ac29f657aeb7be97ae866587d0370eeb9dea7d
ronaldarroyowatson <57762270+ronaldarroyowatson@users.noreply.github.com>
2026-08-02 08:41:51 -0500

    fix: use ESM import for vite react plugin

--- otto-helper/vite.renderer.config.ts
+++ otto-helper/vite.renderer.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import path from 'node:path';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const react = require('@vitejs/plugin-react');
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config
 export default defineConfig({
